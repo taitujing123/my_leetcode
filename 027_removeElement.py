6a6baa2a59450aab8291d1d0b9bbfe5178256e7b
taitujing123 <996067640@qq.com>
2019-06-17 21:58:45 +0800

    Add files via upload

---
+++ 027_removeElement.py
@@ -0,0 +1,30 @@
+"""
+给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        ls = len(nums)
+        if ls == 0:
+            return ls
+        count = 0
+        index = 0
+        while index < ls - count:
+        	if nums[index] == val:
+        		nums[index] == nums[ls - 1 - count]
+        		count += 1
+        	else:
+        		index += 1
+        return ls - count
