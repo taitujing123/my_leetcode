52dd7570f9b64cb067d4d559a5efd8953fed0b7a
taitujing123 <996067640@qq.com>
2019-06-25 09:16:24 +0800

    Add files via upload

---
+++ 058_lengthOfLastWord.py
@@ -0,0 +1,30 @@
+"""
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+
+示例:
+
+输入: "Hello World"
+输出: 5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/length-of-last-word
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        temp = s.split(' ')
+        temp = [t for t in temp if len(t) > 0]
+        if len(temp) == 0:
+            return 0
+        else:
+            return len(temp[-1])
