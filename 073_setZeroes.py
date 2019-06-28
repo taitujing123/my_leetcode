9d9ea86a6964fb19182cba7d90248623eeda90b5
taitujing123 <996067640@qq.com>
2019-06-28 20:26:53 +0800

    Add files via upload

---
+++ 073_setZeroes.py
@@ -0,0 +1,51 @@
+"""
+给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+
+示例 1:
+
+输入: 
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+输出: 
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/set-matrix-zeroes
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix:
+            return
+        m = len(matrix)
+        if m == 0:
+            return
+        r = []
+        c = []
+        n = len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    r.append(i)
+                    c.append(j)
+        # row with zero
+        r = set(r)
+        # column with zero
+        c = set(c)
+        for i in r:
+            for j in range(n):
+                matrix[i][j] = 0
+        for i in range(m):
+            for j in c:
+                matrix[i][j] = 0
