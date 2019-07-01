684195a2fa3924535048166203ff11305e5642fd
taitujing123 <996067640@qq.com>
2019-07-01 21:58:07 +0800

    Add files via upload

---
+++ 082_deleteDuplicate.py
@@ -0,0 +1,40 @@
+"""
+给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+
+示例 1:
+
+输入: 1->2->3->3->4->4->5
+输出: 1->2->5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None or head.next == None:
+            return head
+        dummy = ListNode(-1)
+        dummy.next = head
+        slow = dummy
+        fast = dummy.next
+        while fast:
+            while fast.next and slow.next.val == fast.next.val:
+                fast = fast.next
+            if slow.next == fast:
+                slow = fast
+            else:
+                slow.next = fast.next
+            fast = fast.next
+        return dummy.next
