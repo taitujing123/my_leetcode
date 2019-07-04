d3b316acf42320fd8fc12b3bbffeb09a47e84dfb
taitujing123 <996067640@qq.com>
2019-07-04 19:59:14 +0800

    Add files via upload

---
+++ 101_isSymmetric.py
@@ -0,0 +1,49 @@
+"""
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        return self.mirrorVisit(root.left, root.right)
+
+    def mirrorVisit(self, left, right):
+        if left is None and right is None:
+            return True
+        try:
+            if left.val == right.val:
+                if self.mirrorVisit(left.left, right.right) and self.mirrorVisit(left.right, right.left):
+                    return True
+            return False
+        except:
+            return False
