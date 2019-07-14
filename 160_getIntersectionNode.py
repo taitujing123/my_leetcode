bb2c2817c8b7853185cae03e3104fb05400e5234
taitujing123 <996067640@qq.com>
2019-07-14 20:11:55 +0800

    Add files via upload

---
+++ 160_getIntersectionNode.py
@@ -0,0 +1,36 @@
+"""
+编写一个程序，找到两个单链表相交的起始节点。
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        #two points
+        if not headA or not headB:
+        	return None
+        a, b = headA, headB
+        ans = None
+        while a or b:
+        	if not a:
+        		a = headB
+        	if not b:
+        		b = headA
+        	if a == b and not ans:
+        		ans = a
+        	a, b = a.next, b.next
+        return ans
