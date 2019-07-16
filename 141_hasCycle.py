81a88833ee02ff7115882ad73d185cd2f7f2069f
taitujing123 <996067640@qq.com>
2019-07-16 09:06:27 +0800

    Add files via upload

---
+++ 141_hasCycle.py
@@ -0,0 +1,33 @@
+"""
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        try:
+        	fast = head.next.next
+        	slow = head.next
+
+        	while fast != slow:
+        		fast = fast.next.next
+        		slow = slow.next
+
+        	return True
+        except:
+        	return False
+        
