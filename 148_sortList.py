0d31af7bf1892b45b33fa5eacc43f1d8724821b7
taitujing123 <996067640@qq.com>
2019-07-17 09:31:13 +0800

    Add files via upload

---
+++ 148_sortList.py
@@ -0,0 +1,46 @@
+"""
+在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+
+示例 1:
+
+输入: 4->2->1->3
+输出: 1->2->3->4
+示例 2:
+
+输入: -1->5->3->4->0
+输出: -1->0->3->4->5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+        	return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+        	fast, slow = fast.next.next, slow.next
+        mid, slow.next = slow.next, None
+        left, right = self.sortList(head), self.sortList(mid)
+        h = res = ListNode(0)
+        while left and right:
+        	if left.val < right.val:
+        		h.next = left
+        		left = left.next
+        	else:
+        		h.next = right
+        		right = right.next
+        	h = h.next
+        h.next = left if left else right
+        return res.next
