4751fbd15de0732e778300f4b93323a6ec0ff38a
taitujing123 <996067640@qq.com>
2019-07-22 20:48:10 +0800

    Add files via upload

---
+++ 189_rotate.py
@@ -0,0 +1,64 @@
+"""
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rotate-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        for i in range(k):
+            nums.insert(0,nums.pop())
+        
+        return nums
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        if n<=0:
+            return False
+        elif n==1:
+            return nums
+        else:
+            k=k%n
+            self.reverse(nums,0,n-1)
+            self.reverse(nums,0,k-1)
+            self.reverse(nums,k,n-1)
+        return nums
+    def reverse(self,nums,start,end):
+        while start<end:
+            nums[start],nums[end]=nums[end],nums[start]
+            start+=1
+            end-=1
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        t=nums[-k:]
+        nums[k:]=nums[:-k]
+        nums[:k]=t
