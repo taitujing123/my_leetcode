eb7c40856e39f77b19a41ea14a304e5322d638e0
taitujing123 <996067640@qq.com>
2019-07-22 15:33:38 +0800

    Add files via upload

---
+++ 560_subarraySum.py
@@ -0,0 +1,33 @@
+"""
+给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+
+示例 1 :
+
+输入:nums = [1,1,1], k = 2
+输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+说明 :
+
+数组的长度为 [1, 20,000]。
+数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sum_map = {}
+        sum_map[0] = 1
+        count = curr_sum = 0
+        for num in nums:
+            curr_sum += num
+            # Check if sum - k in hash
+            count += sum_map.get(curr_sum - k, 0)
+            # add curr_sum to hash
+            sum_map[curr_sum] = sum_map.get(curr_sum, 0) + 1
+        return count
