2cf560f5749483edba14a1cd0f1be18debcaec12
taitujing123 <996067640@qq.com>
2019-08-13 21:13:15 +0800

    Add files via upload

---
+++ 203_removeElements.py
@@ -0,0 +1,30 @@
+"""
+删除链表中等于给定值 val 的所有节点。
+
+示例:
+
+输入: 1->2->6->3->4->5->6, val = 6
+输出: 1->2->3->4->5
+"""
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        # add a extra head for removing head
+        prehead = ListNode(-1)
+        prehead.next = head
+        last, pos = prehead, head
+        while pos is not None:
+            if pos.val == val:
+                last.next = pos.next
+            else:
+                last = pos
+            pos = pos.next
+        return prehead.next
