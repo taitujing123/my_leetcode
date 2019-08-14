68f96d8654834a40f9a7f216ac3f6f5d04197ab0
taitujing123 <996067640@qq.com>
2019-08-14 09:57:45 +0800

    Add files via upload

---
+++ 328_oddEvenList.py
@@ -0,0 +1,48 @@
+"""
+是节点的值的奇偶性。
+
+请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+
+示例 1:
+
+输入: 1->2->3->4->5->NULL
+输出: 1->3->5->2->4->NULL
+示例 2:
+
+输入: 2->1->3->5->6->4->7->NULL 
+输出: 2->3->6->7->1->5->4->NULL
+说明:
+
+应当保持奇数节点和偶数节点的相对顺序。
+链表的第一个节点视为奇数节点，第二个节点视为偶数节点，以此类推。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/odd-even-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None:
+            return head
+        oddhead, evenhead = head, head.next
+        p1, p2 = oddhead, evenhead
+        while p1.next is not None and p2.next is not None:
+            p1.next = p2.next
+            p1 = p2.next
+            p2.next = p1.next
+            p2 = p1.next
+        
+        p1.next = evenhead
+        
+        return oddhead
+            
