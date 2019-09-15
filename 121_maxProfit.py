a8c4976423ed725eaafa3ad558742345d23da376
taitujing123 <996067640@qq.com>
2019-09-15 14:23:20 +0800

    Add files via upload

--- 121_maxProfit.py
+++ 121_maxProfit.py
@@ -5,38 +5,29 @@
 
 注意你不能在买入股票前卖出股票。
 
-示例 1:
-
-输入: [7,1,5,3,6,4]
-输出: 5
-解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
-     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
-示例 2:
-
-输入: [7,6,4,3,1]
-输出: 0
-解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
-
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+
+
 class Solution(object):
     def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
         """
-        length = len(prices)
-        if length == 0:
+        if not prices:
             return 0
-        max_profit, low = 0, prices[0]
-        for i in range(1, length):
-            if low > prices[i]:
-                low = prices[i]
-            else:
-            	temp = prices[i] - low
-            	if temp > max_profit:
-                    max_profit = temp
-        return max_profit
+        res = 0
+        profit = [[0 for i in range(3)] for i in range(len(prices))]
 
+        # 第二维的0，1，2分别表示无购买股票，购入股票，已经卖出股票
+        profit[0][0], profit[0][1], profit[0][2] = 0, -prices[0], 0
+        for i in range(1, len(prices)):
+            profit[i][0] = profit[i - 1][0]
+            profit[i][1] = max(profit[i - 1][1], profit[i - 1][0] - prices[i])
+            profit[i][2] = profit[i - 1][1] + prices[i]
+            res = max(res, profit[i][0], profit[i][1], profit[i][2])
+
+        return res
