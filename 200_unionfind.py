a5c422d9d0addd9596809c3db69e2e7d937654ac
taitujing123 <996067640@qq.com>
2019-09-15 21:38:56 +0800

    Add files via upload

---
+++ 200_unionfind.py
@@ -0,0 +1,76 @@
+"""
+给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+
+示例 1:
+
+输入:
+11110
+11010
+11000
+00000
+
+输出: 1
+示例 2:
+
+输入:
+11000
+11000
+00100
+00011
+
+输出: 3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class UnionFind(object):
+    def __init__(self, grid):
+        m, n = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [-1] * (m * n)
+        self.rank = [0] * (m * n)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.parent[i * n + j] = i * n + j
+                    self.count += 1
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rootx]:
+                self.parent[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+        uf = UnionFind(grid)
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '0':
+                    continue
+                for d in direction:
+                    nr, nc = i + d[0], j + d[1]
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == '1':
+                        uf.union(i * n + j, nr * n + nc)
+
+        return uf.count
