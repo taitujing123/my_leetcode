d8d8035882ef0af78d3d24ada4ba8dc56b5c2fd2
taitujing123 <996067640@qq.com>
2019-09-15 15:00:25 +0800

    Add files via upload

--- 123_maxProfit.py
+++ 123_maxProfit.py
@@ -1,31 +1,12 @@
 """
-给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
 
-设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
 
-注意: 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
-
-示例 1:
-
-输入: [3,3,5,0,0,3,1,4]
-输出: 6
-解释: 在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
-     随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
-示例 2:
-
-输入: [1,2,3,4,5]
-输出: 4
-解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。   
-     注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。   
-     因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
-示例 3:
-
-输入: [7,6,4,3,1] 
-输出: 0 
-解释: 在这个情况下, 没有交易完成, 所以最大利润为 0。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 class Solution(object):
@@ -34,35 +15,27 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # 计算left_max
-        left_max = [0 for i in range(len(prices))]
-        current_min = 2 ** 16
-        tmp_max = 0
-        for idx, p in enumerate(prices):
-            if p - current_min > tmp_max:
-                tmp_max = p - current_min
-            left_max[idx] = tmp_max
-            if p < current_min:
-                current_min = p
+        if not prices:
+            return 0
+        profit = [[[0 for _ in range(2)] for _ in range(3)] for _ in range(len(prices))]
 
-        # 计算right_max
-        right_max = [0 for i in range(len(prices))]
-        current_max = 0
-        tmp_max = 0
-        for i in range(len(prices) - 1, -1, -1):
-            p = prices[i]
-            if current_max - p > tmp_max:
-                tmp_max = current_max - p
-            right_max[i] = tmp_max
-            if p > current_max:
-                current_max = p
+        #第一维表示天数，第二维表示完成几笔交易，第三纬度表示是否持有股票
 
-        # 计算max_earn
-        max_earn = 0
-        for i in range(len(prices)):
-            left = left_max[i]
-            right = right_max[i + 1] if i + 1 < len(prices) else 0
-            max_ = left + right
-            if max_ > max_earn:
-                max_earn = max_
-        return max_earn
+        profit[0][0][0], profit[0][0][1] = 0, -prices[0]
+        profit[0][1][0], profit[0][1][1] = float('-inf'), float('-inf')
+        profit[0][2][0], profit[0][2][1] = float('-inf'), float('-inf')
+
+        for i in range(1, len(prices)):
+            profit[i][0][0] = profit[i-1][0][0]
+            profit[i][0][1] = max(profit[i-1][0][1], profit[i-1][0][0] - prices[i])
+
+            profit[i][1][0] = max(profit[i-1][1][0], profit[i-1][0][1] + prices[i])
+            profit[i][1][1] = max(profit[i-1][1][1], profit[i-1][1][0] - prices[i])
+
+            profit[i][2][0] = max(profit[i-1][2][0], profit[i-1][1][1] + prices[i])
+
+        end = len(prices) - 1
+        return max(profit[end][0][0], profit[end][1][0], profit[end][2][0])
+
+
+
