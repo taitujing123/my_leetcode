1c77d0afe7033e46811f93031b5cca8c0b80d418
TT_taitujing <996067640@qq.com>
2019-09-18 23:16:11 +0800

    Add files via upload

---
+++ 25_reverKGroup.py
@@ -0,0 +1,57 @@
+"""
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+示例 :
+
+给定这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+说明 :
+
+你的算法只能使用常数的额外空间。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+来源：力扣（LeetCode)
+链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        p = dummy
+        while True:
+            count = k
+            tmp = head
+            stack = []
+            while count and tmp:
+                stack.append(tmp)
+                tmp = tmp.next
+                count -= 1
+            if count:
+                p.next = head
+                break
+            while stack:
+                p.next = stack.pop()
+                p = p.next
+            p.next = tmp
+            head = tmp
+
+        return dummy.next
