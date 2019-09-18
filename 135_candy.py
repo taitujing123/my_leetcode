2318c70e950d36ce47df940109951bcaff5cf439
TT_taitujing <996067640@qq.com>
2019-09-18 22:26:22 +0800

    Update 135_candy.py

--- 135_candy.py
+++ 135_candy.py
@@ -40,3 +40,23 @@ class Solution(object):
             if ratings[i - 1] > ratings[i]:
                 num[i - 1] = max(num[i] + 1, num[i - 1])
         return sum(num)
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        res = [1 for _ in range(n)]
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                res[i] = res[i - 1] + 1
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1] and res[i] <= res[i+1]:
+                res[i] = res[i + 1] + 1
+
+        return sum(res)
+if __name__ == '__main__':
+    s = Solution()
+    s.candy([1, 0, 2])
