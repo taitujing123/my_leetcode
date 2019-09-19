ef4562bc55e3e81253894783563ef8bb74b35f46
TT_taitujing <996067640@qq.com>
2019-09-19 20:31:53 +0800

    Update 556_nextGreaterElement.py

--- 556_nextGreaterElement.py
+++ 556_nextGreaterElement.py
@@ -20,19 +20,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        nums = [int(i) for i in str(n)]
+        nums = [int(x) for x in str(n)]
         if sorted(nums)[::-1] == nums:
-        	return -1
+            return -1
         m = len(nums)
-        for i in range(m-2,-1,-1):
-        	if nums[i] < nums[i+1]:
-        		for j in range(m-1,i,-1):
-        			if nums[j] > nums[i]:
-        				nums[i], nums[j] = nums[j], nums[i]
-        				nums[i+1:] = sorted(nums[i+1:])
-        			break
-        	break
+        for i in range(m - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                for j in range(m - 1, i, -1):
+                    if nums[j] > nums[i]:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        nums[i + 1:] = sorted(nums[i + 1:])
+                        break
+                break
         res = 0
         for i in nums:
-        	res = 10 * res + i
-        return res if res < 2**31 else -1
+            res = 10 * res + i
+        return res if res<2**31 else -1
